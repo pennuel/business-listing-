0257eef70e9996ee2cb8f5deec02632dd89bf0f1
PAUL PENNUEL ECIL <56873997+pennuel@users.noreply.github.com>
2025-08-17 22:27:23 +0300

    fix: creation of the server routes and the helper functions

--- lib/server-auth.ts
+++ lib/server-auth.ts
@@ -1,12 +1,12 @@
-import { getServerSession } from "next-auth"
-import { authOptions } from "./auth"
+import { getServerSession } from "next-auth";
+import { authOptions } from "./auth";
 
 /**
  * Wrapper for getting the server session in server components or API routes.
  * Usage: const session = await getServerAuthSession()
  */
 export async function getServerAuthSession() {
-  return await getServerSession(authOptions)
+  return await getServerSession(authOptions);
 }
 
-export default getServerAuthSession
+export default getServerAuthSession;
