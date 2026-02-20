da65639433bc4ed6b9a730354641c734ff898a49
PAUL PENNUEL ECIL <paulpennuel@gmail.com>
2026-02-20 10:43:35 +0300

    feat: configured to work with the new database config

--- apps/business-listing/next.config.mjs
+++ apps/business-listing/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  transpilePackages: ["@think-id/database", "@think-id/ui"],
   eslint: {
     ignoreDuringBuilds: true,
   },
