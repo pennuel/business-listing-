2f1eb815060033d584e6282a8e1b5e785c756844
PAUL PENNUEL ECIL <paulpennuel@gmail.com>
2026-02-25 12:10:30 +0300

    chore: updated the components to work with the new updated version of tailwind and shadcn, tanstack

--- apps/business-listing/next.config.mjs
+++ apps/business-listing/next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  transpilePackages: ["@think-id/database", "@think-id/ui"],
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  transpilePackages: ["@think-id/database", "@think-id/ui", "@think-id/types"],
   typescript: {
     ignoreBuildErrors: true,
   },
